db2a92653ac7ffebf3bcf6639d9079c1b767fdeb
Shad02w <alvistse123@gmail.com>
2023-11-10 10:15:43 -0500

    fix: remove fake createApi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer } from './productList'
 import { useDispatch } from 'react-redux'
-import { fakeStoreAPI } from '../api'
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
@@ -9,10 +8,6 @@ export type AppDispatch = typeof store.dispatch
 export const store = configureStore({
     reducer: {
         products: productListReducer,
-        [fakeStoreAPI.reducerPath]: fakeStoreAPI.reducer,
-    },
-    middleware: getDefaultMiddleware => {
-        return getDefaultMiddleware().concat(fakeStoreAPI.middleware)
     },
 })
 
