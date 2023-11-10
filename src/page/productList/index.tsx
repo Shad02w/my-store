119c3a44cafd82344923fda62019768ffc58ee02
Shad02w <alvistse123@gmail.com>
2023-11-10 11:34:33 -0500

    refactor: add uniqueId instead replace id of fakeApi /products item

--- src/page/productList/index.tsx
+++ src/page/productList/index.tsx
@@ -1,23 +1,41 @@
 import './index.scss'
-import { fetchNextProductPage, useProductsState } from '../../store/productList'
+import { productListActions, useProductsState } from '../../store/productList'
 import { useAppDispatch } from '../../store'
 import { FlatList } from '../../component/FlatList'
 import CartIcon from '../../asset/cart.svg?react'
 import { Footer } from './Footer'
+import { Link } from 'react-router-dom'
+import { Badge } from '../../component/Badge'
+import { cartActions, useCartState } from '../../store/cart'
+import { useMemo } from 'react'
 
 export function ProductList() {
     const { loading, error, products } = useProductsState()
+    const { items } = useCartState()
     const dispatch = useAppDispatch()
 
-    const fetchNextPage = () => dispatch(fetchNextProductPage())
+    const productsToBeShown = useMemo(() => products.filter(_ => !items.includes(_.uniqueId)), [items, products])
+
+    const fetchNextPage = () => dispatch(productListActions.fetchNextPage())
+    const addToCart = (id: string) => {
+        dispatch(cartActions.add(id))
+        dispatch(productListActions.remove(id))
+    }
 
     return (
         <div id="product-main">
-            <h3>Product List</h3>
+            <div className="header">
+                <h3>Product List</h3>
+                <Link to="/cart">
+                    <Badge count={items.length}>
+                        <CartIcon />
+                    </Badge>
+                </Link>
+            </div>
             <FlatList
                 id="product-list"
-                data={products}
-                keyExtractor={_ => _.id}
+                data={productsToBeShown}
+                keyExtractor={_ => _.uniqueId}
                 onEndReach={loading ? undefined : fetchNextPage}
                 renderItem={product => (
                     <div className="product-card" key={product.id}>
@@ -25,7 +43,7 @@ export function ProductList() {
                         <div className="title">{product.title}</div>
                         <p>{product.description}</p>
                         <span>${product.price}</span>
-                        <button>
+                        <button onClick={() => addToCart(product.uniqueId)}>
                             <CartIcon />
                         </button>
                     </div>
--- src/store/productList.ts
+++ src/store/productList.ts
@@ -1,13 +1,19 @@
-import { SerializedError, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { Product, getProducts } from '../api'
+import { PayloadAction, SerializedError, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { Product as APIProductType, getProducts } from '../api'
 import { useSelector } from 'react-redux'
 import { RootState } from './index'
 
-interface ProductsState {
+interface Product extends APIProductType {
+    uniqueId: string
+}
+
+export interface ProductsState {
     loading: boolean
     products: Array<Product>
     error: SerializedError | null
 }
+
+const fetchNextProductPage = createAsyncThunk('products/fetchNextPage', getProducts)
 
 const initialState: ProductsState = {
     loading: false,
@@ -15,23 +21,25 @@ const initialState: ProductsState = {
     error: null,
 }
 
-export const fetchNextProductPage = createAsyncThunk('products/fetchNextPage', getProducts)
-
 const products = createSlice({
     name: 'products',
     initialState,
-    reducers: {},
+    reducers: {
+        remove(state, action: PayloadAction<string>) {
+            state.products = state.products.filter(product => product.uniqueId !== action.payload)
+        },
+    },
     extraReducers: builder => {
         builder.addCase(fetchNextProductPage.fulfilled, (state, action) => {
             // Since fakeStoreAPI only returns 20 products, we need to change the id to a random string,
             // prevent the key from being reused
-            state.products = [...state.products, ...action.payload.map(_ => ({ ..._, id: createKey() }))]
+            state.products = [...state.products, ...action.payload.map(_ => ({ ..._, uniqueId: createKey() }))]
             state.loading = false
-            state.error = null
         })
 
         builder.addCase(fetchNextProductPage.pending, state => {
             state.loading = true
+            state.error = null
         })
 
         builder.addCase(fetchNextProductPage.rejected, (state, action) => {
@@ -41,10 +49,13 @@ const products = createSlice({
     },
 })
 
+export const productListReducer = products.reducer
+export const productListActions = {
+    ...products.actions,
+    fetchNextPage: fetchNextProductPage,
+}
+export const useProductsState = () => useSelector((state: RootState) => state.products)
+
 function createKey() {
     return Math.random().toString(36).substr(2, 9)
 }
-
-export const productListReducer = products.reducer
-export const productListActions = products.actions
-export const useProductsState = () => useSelector((state: RootState) => state.products)
